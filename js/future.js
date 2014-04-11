02f78e8f6fb1d6d1761ee708660ced5d26288063
piumosso <golikoffartem@gmail.com>
2014-04-12 01:38:36 +0400

    Прозрачности

--- js/future.js
+++ js/future.js
@@ -7,16 +7,26 @@ futureApp.controller('glowController', function($scope){
         return _.random(from || to || 0, to || from || 0);
     }
 
-    $scope.$watchCollection('[widthFrom, widthTo, hueFrom, hueTo, saturationFrom, saturationTo, lightnessFrom, lightnessTo]', function(){
-        console.table([$scope])
+    function getColor(h, s, l, a) {
+        return 'hsla(' + h + ', ' + s + '%, ' + l + '%, ' + a + ')';
+    }
 
-        $scope.glow = _.map(_.range(400), function(){
-            var item = {
+    $scope.$watchCollection('[widthFrom, widthTo, heightFrom, heightTo, hueFrom, hueTo, saturationFrom, saturationTo, lightnessFrom, lightnessTo, gradient, gradientPoint]', function(){
+        $scope.glow = _.map(_.range(500), function(){
+            var item,
+                colors,
+                hue,
+                saturation,
+                lightness;
+
+            hue = random(+$scope.hueFrom, +$scope.hueTo);
+            saturation = random(+$scope.saturationFrom, +$scope.saturationTo);
+            lightness = random(+$scope.lightnessFrom, +$scope.lightnessTo);
+            colors = [getColor( hue, saturation, lightness, 1), getColor( hue, saturation, lightness, 0)];
+            item = {
                 width: random(+$scope.widthFrom, +$scope.widthTo),
-                heigth: 300,
-                hue: random(+$scope.hueFrom, +$scope.hueTo),
-                saturation: random(+$scope.saturationFrom, +$scope.saturationTo),
-                lightness: random(+$scope.lightnessFrom, +$scope.lightnessTo)
+                heigth: random(+$scope.heightFrom, +$scope.heightTo),
+                background: $scope.gradient ? 'linear-gradient(to bottom, ' + colors[0] + ' 0%, ' + colors[0] + ' ' + $scope.gradientPoint + '%, ' + colors[1] + ' 100%);' : colors[0]
             };
 
             return  item;
@@ -26,11 +36,16 @@ futureApp.controller('glowController', function($scope){
     $scope.initialize = function(){
         $scope.widthFrom = 2;
         $scope.widthTo = 5;
+        $scope.heightFrom = 230;
+        $scope.heightTo = 300;
         $scope.hueFrom = 1;
         $scope.hueTo = 50;
         $scope.saturationFrom = 50;
         $scope.saturationTo = 50;
         $scope.lightnessFrom = 50;
         $scope.lightnessTo = 50;
+        $scope.opacity = 1;
+        $scope.gradient = true;
+        $scope.gradientPoint = 50;
     };
 });
